1c1db10483d27be62cc6f57e1ec5391d5235e02c
ParthShitole <f20212907@goa.bits-pilani.ac.in>
2023-07-07 10:22:17 +0000

    minor changes

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const menu = document.getElementById("mobile-menu");
 const menuLinks = document.querySelector(".nav-menu");
 const cover= document.getElementById("cover");
-
+const review = document.getElementById("review")
 
 menu.addEventListener('click',function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
     cover.classList.toggle('im-active');
+    review.classList.toggle('disappear');
 })
 
